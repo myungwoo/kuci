5b28176e3b1e1b63b0fd2590389c36babccb39a7
myungwoo <mc.tamaki@gmail.com>
2015-04-16 17:31:18 +0900

    add default setting files

---
+++ kuci/kuci_settings.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+gmail_username = u'고려대학교 정보대학 학생회'
+gmail_user = 'kuci.bon@gmail.com'
+gmail_pwd = '' # gmail password
+
+coolsms_username = 'kuci'
+coolsms_pwd = '' # coolsms password
+
+def code_check_sms_content(number, name, phone_number, code):
+	return '''[고려대학교 정보대학 학생회]
+%s님의 본인 인증 코드: %s'''%(name.encode('utf8'), code.encode('utf8'))
+
+code_check_email_subject = u'고려대학교 정보대학 학생회 홈페이지 인증 확인 메일'
+
+def code_check_email_content(email, code):
+	return u'''안녕하세요.<br/>
+고려대학교 정보대학 제1대 학생회 本 입니다.<br/>
+회원 가입을 위해 메일 인증을 해야합니다.<br/>
+메일 인증 코드는 아래와 같습니다.<br/>
+<br/>
+%s<br/>
+<br/>
+가입해주셔서 감사합니다.</br>
+민족고대 첨단정보 제1대 학생회 本 드림<'''%(code)
---
+++ kuci/settings.py
@@ -0,0 +1,118 @@
+"""
+Django settings for kuci project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.contrib.messages.context_processors.messages',
+    'kuci.context_processors.default',
+)
+
+# SESSION COOKIE SETTINGS
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+SESSION_COOKIE_AGE = 1 * 60 * 60 # in seconds
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '' # Django secret key
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1', 'kuci.korea.ac.kr']
+
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), '../templates'),
+)
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+#    'django.contrib.staticfiles',
+    'members',
+    'board',
+    'poll',
+    'south',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+#    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'kuci.urls'
+
+WSGI_APPLICATION = 'kuci.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'kuci',                      # Or path to database file if using sqlite3.
+        'USER': 'db_username',                      # Not used with sqlite3.
+        'PASSWORD': 'db_password',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'ko-KR'
+
+TIME_ZONE = 'Asia/Seoul'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = False
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+#    os.path.join(os.path.dirname(__file__), './../static'),
+)
+
+# Media files
+
+MEDIA_URL = '/'
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), '../database')
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGIN_URL = '/login/'
+
+LOGOUT_URL = '/logout/'
